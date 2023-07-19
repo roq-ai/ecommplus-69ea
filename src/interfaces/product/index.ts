d72672921b0bac26f72969bd50908d10559a6777
roq-ai <>
2023-07-19 16:05:36 +0000

    Initial commit

---
+++ src/interfaces/product/index.ts
@@ -0,0 +1,23 @@
+import { VendorInterface } from 'interfaces/vendor';
+import { GetQueryInterface } from 'interfaces';
+
+export interface ProductInterface {
+  id?: string;
+  name: string;
+  description?: string;
+  image?: string;
+  vendor_id?: string;
+  created_at?: any;
+  updated_at?: any;
+
+  vendor?: VendorInterface;
+  _count?: {};
+}
+
+export interface ProductGetQueryInterface extends GetQueryInterface {
+  id?: string;
+  name?: string;
+  description?: string;
+  image?: string;
+  vendor_id?: string;
+}
